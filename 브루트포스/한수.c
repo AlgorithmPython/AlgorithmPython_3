0cf6c1295439aa7226742c06a9f79bf17b239556
imsmile2000 <69185594+imsmile2000@users.noreply.github.com>
2022-05-19 19:43:05 +0900

    Update 한수.c

--- 브루트포스/한수.c
+++ 브루트포스/한수.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int hansu(int x){
+    int count=0;
+    if(x<100){
+        return x;
+    }else{
+        for(int i=100;i<=x;i++){
+            int a=(i%100)%10;
+            int b=(i%100)/10;
+            int c=(i/100);
+            if(2*b==a+c){
+                count++;
+            }
+        }
+        return (count+99);
+    }
+}
+int main(){
+    int x;
+    scanf("%d",&x);
+    printf("%d",hansu(x));
+}
